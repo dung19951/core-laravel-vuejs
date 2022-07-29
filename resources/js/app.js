ab44d442067d8be71b825d5b690cf104a2813ccf
DungNguyen95 <83797495+dung19951@users.noreply.github.com>
2022-07-29 22:50:01 +0700

    1

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,8 +7,11 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const app = createApp(App)
+app.use(VueSweetalert2)
 app.use(router)
 app.use(VueAxios, axios)
 app.use(ElementPlus)
